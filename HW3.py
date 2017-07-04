1e3a629379c395fd7dcf684ddd16fab281b7ee56
Dmitriy Voitik <voitikd@gmail.com>
2017-07-05 02:00:33 +0300

    upd honework_3

--- HW3.py
+++ HW3.py
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
+
 #Functions
 #1
 def sum_not_sum(x, y):
@@ -11,3 +19,24 @@ def sum_not_sum(x, y):
 print(sum_not_sum(-2, -12))
 
 #2
+def my_string(my_string, case=True):
+    if case:
+    	return my_string.upper()
+    else:
+    	return my_string.lower()
+
+print(my_string('python is a beatiful language'))
+print(my_string('AAAABBBBBCCCC', False))    	
+
+
+#3
+def glue_func(*args, glue=':'):
+    for i in (args):
+        if len(i) > 3:
+    	    print(i, glue)
+        else: continue    
+
+glue_func('12', 'asdff', '4312', '12', 'qwerty')
+
+# реализовать печать в одну строку, а не построчно!
+
